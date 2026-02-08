c973dab812aaff98b5d5ec6ff5add1320f568a5b
somang <s2_somang@naver.com>
2026-02-08 17:18:47 +0900

    7강-신기하네

--- chatgpt-clone/main.py
+++ chatgpt-clone/main.py
@@ -1,6 +1,41 @@
-def main():
-    print("Hello from chatgpt-clone!")
+import streamlit as st
+
+import time
+
+st.header("하이~~ ")
+
+st.button("클릭")
+
+st.text_input("너의 api key 입력해", max_chars=20 )
+
+st.feedback("faces")
+
+with st.sidebar:
+    st.badge("Badge 1")
 
 
-if __name__ == "__main__":
-    main()
+tab1, tab2, tab3 = st.tabs(["Agent", "Chat","Output"])
+
+with tab1:
+    st.header("Agent")
+with tab2:
+    st.header("Agent 2")
+with tab3:
+    st.header("Agent 3")
+
+
+with st.chat_message("ai"):
+    st.text("하이")
+    with st.status('Agent is using tool')as status:
+        time.sleep(1)
+        status.update(label="Agent is searching the web...")
+        time.sleep(2)
+        status.update(label="Agent is reading the page...")
+        time.sleep(3)
+        status.update(state="complete")
+
+with st.chat_message("human"):
+    st.text("하이~~")
+    
+
+st.chat_input("write a message form the assistant", accept_file=True)
